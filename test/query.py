dbf25cb1723a586bfd0880f2686b9f8da4a1e717
vlad107 <vlad10795@gmail.com>
2016-12-17 12:43:00 +0300

    add script for testing

---
+++ test/query.py
@@ -0,0 +1,23 @@
+import socket
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5555
+BUFFER_SIZE = 1024
+MESSAGE = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n"
+
+all_data = []
+for num in range(100):
+	print "connection #", num
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((TCP_IP, TCP_PORT))
+	s.send(MESSAGE)
+	data = s.recv(BUFFER_SIZE)
+	s.close()
+	print "done"
+	all_data += [data]
+print all_data[0]
+for i in range(1, len(all_data)):
+	if (all_data[i] != all_data[i - 1]):
+		print "answers for the same query are different"
+		exit(0)
+print "Ok!"
